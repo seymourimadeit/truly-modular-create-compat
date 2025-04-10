f7c06313e62b48aa9deab8a52d65d95f6e301d4c
seymourimadeit <saulgoodman53434344@gmail.com>
2025-04-10 10:30:18 -0700

    fixed crash

--- common/src/main/java/tallestegg/truly_modular_create_compat/mixins/DivingHelmetMixin.java
+++ common/src/main/java/tallestegg/truly_modular_create_compat/mixins/DivingHelmetMixin.java
@@ -15,6 +15,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import tallestegg.truly_modular_create_compat.TrulyModularCreateCompatModCommon;
+
 @Mixin(DivingHelmetItem.class)
 public abstract class DivingHelmetMixin extends BaseArmorItem {
 
@@ -22,14 +23,8 @@ public abstract class DivingHelmetMixin extends BaseArmorItem {
         super(armorMaterial, type, properties, textureLoc);
     }
 
-    @Inject(at = @At(value = "RETURN"), method = "getWornItem", cancellable = true)
-    private static void getWornItem(Entity entity, CallbackInfoReturnable<ItemStack> cir) {
-     //   cir.setReturnValue(TrulyModularCreateCompatModCommon.getWornItem(entity));
-    }
-
-    @ModifyVariable(method = "breatheUnderwater", at = @At(value = "STORE"), ordinal = -1)
-    private static ItemStack breathe(ItemStack value, @Local(ordinal = 0) LivingEntity entity){
+    @ModifyVariable(method = "breatheUnderwater", at = @At(value = "STORE"), remap = false)
+    private static ItemStack breathe(ItemStack value, @Local(ordinal = 0) LivingEntity entity) {
         return TrulyModularCreateCompatModCommon.getWornItem(entity);
     }
-
 }
